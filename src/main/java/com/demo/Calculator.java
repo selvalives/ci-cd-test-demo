696ce41604577c9ac4af5d5493d1c94a3c109225
Selvakumar Arunasalam <selvalives@gmail.com>
2026-03-12 09:02:43 +0800

    Corrected the logic

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -1,8 +1,9 @@
 package com.demo;
 
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b + 1;
+public class Calculator 
+{
+    public int add(int a, int b) 
+    {
+        return a + b;
     }
 }
--- src/test/java/com/demo/CalculatorTest.java
+++ src/test/java/com/demo/CalculatorTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+public class CalculatorTest 
+{
 
     @Test
-    public void testAdd() {
+    public void testAdd() 
+    {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result);
